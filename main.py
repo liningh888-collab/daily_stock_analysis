78194323fae99255fab57610f36f171ba4b918c9
liningh888-collab <liningh888@gmail.com>
2026-07-31 12:26:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def get_standard_now():
 def get_time_type():
     now = get_standard_now()
     h, m = now.hour, now.minute
-    if h == 7 and m == 40:
+    if h == 7 and m == 55:
         return "morning"
     elif h == 9 and m == 30:
         return "open"
